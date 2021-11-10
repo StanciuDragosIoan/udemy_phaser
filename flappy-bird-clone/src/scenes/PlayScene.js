4c198c180ecb9c86a29c813b43c9d41c8e06e4cc
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-11-10 10:57:15 +0200

    test

--- flappy-bird-clone/src/scenes/PlayScene.js
+++ flappy-bird-clone/src/scenes/PlayScene.js
@@ -46,14 +46,21 @@ class PlayScene extends Phaser.Scene {
       .setOrigin(0);
 
     this.bird.body.gravity.y = 400;
+    this.bird.body.setCollideWorldBounds();
   }
 
   createPipes() {
     this.pipes = this.physics.add.group();
 
     for (let i = 0; i < PIPES_TO_RENDER; i++) {
-      const upperPipe = this.pipes.create(0, 0, "pipe").setOrigin(0, 1);
-      const lowerPipe = this.pipes.create(0, 0, "pipe").setOrigin(0, 0);
+      const upperPipe = this.pipes
+        .create(0, 0, "pipe")
+        .setImmovable(true)
+        .setOrigin(0, 1);
+      const lowerPipe = this.pipes
+        .create(0, 0, "pipe")
+        .setImmovable(true)
+        .setOrigin(0, 0);
 
       this.placePipe(upperPipe, lowerPipe);
     }
@@ -71,7 +78,10 @@ class PlayScene extends Phaser.Scene {
   }
 
   checkGameStatus() {
-    if (this.bird.y > this.config.height || this.bird.y < -this.bird.height) {
+    if (
+      this.bird.getBounds().bottom >= this.config.height ||
+      this.bird.y <= 0
+    ) {
       this.gameOver();
     }
   }
@@ -119,9 +129,11 @@ class PlayScene extends Phaser.Scene {
   }
 
   gameOver() {
-    this.bird.x = this.config.startPosition.x;
-    this.bird.y = this.config.startPosition.y;
-    this.bird.body.velocity.y = 0;
+    // this.bird.x = this.config.startPosition.x;
+    // this.bird.y = this.config.startPosition.y;
+    // this.bird.body.velocity.y = 0;
+    this.physics.pause();
+    this.bird.setTint(0xee4824);
   }
 
   flap() {
